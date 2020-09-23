cb271f0263d40c9e0937c9cb8929d9ccd81b3a57
Maksym Kovalenko <m.v.kovalenko4@gmail.com>
2020-09-23 11:26:25 +0300

    Updates

---
+++ precache-manifest.752df231d52a05d0402f3a073198ecc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126e1547fc3e428d832e23cca8535443",
+    "url": "/GOIT-FE-COURSE/index.html"
+  },
+  {
+    "revision": "b6f0ba19df91fe7f9201",
+    "url": "/GOIT-FE-COURSE/static/css/main.4d0f97ca.chunk.css"
+  },
+  {
+    "revision": "42fd20173d163e28dd39",
+    "url": "/GOIT-FE-COURSE/static/js/2.237cc268.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GOIT-FE-COURSE/static/js/2.237cc268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f0ba19df91fe7f9201",
+    "url": "/GOIT-FE-COURSE/static/js/main.41e618b9.chunk.js"
+  },
+  {
+    "revision": "6ab9d91825651bd1a099",
+    "url": "/GOIT-FE-COURSE/static/js/runtime-main.45c31d6a.js"
+  }
+]);
